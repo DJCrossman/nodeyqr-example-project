0307099828a6e939181131ec449fcc48a72c7437
DJCrossman <david@crossman.io>
2019-11-21 14:17:48 -0600

    Completed Assignment 7
    - Show at least 3 screens implemented with mock API

---
+++ js/scripts.js
@@ -0,0 +1,133 @@
+window.onload = () => {
+  let user = null
+  let matches = []
+  let dogs = []
+  let updateDogCard = (dog) => {
+    let dogCard = document.querySelector('.dog-card')
+    if (dogCard && dog) {
+      dogCard.style.backgroundImage = `url(${dog.picture})`
+      dogCard.innerHTML = ''
+      const div = document.createElement('div')
+      div.classList.add('mt-auto', 'text-white', 'p-3')
+      const h2 = document.createElement('h2')
+      h2.innerText = dog.name
+      div.appendChild(h2)
+      const p = document.createElement('p')
+      p.innerText = dog.description
+      div.appendChild(p)
+      dogCard.appendChild(div)
+    } else {
+      dogCard.style.backgroundImage = null
+      dogCard.innerHTML = ''
+      const div = document.createElement('div')
+      div.classList.add('mt-auto', 'p-3')
+      const h3 = document.createElement('h3')
+      h3.innerText = 'No dogs in your area.'
+      div.appendChild(h3)
+      const p = document.createElement('p')
+      p.innerText = 'Please check back later'
+      div.appendChild(p)
+      dogCard.appendChild(div)
+    }
+  }
+  let updateMatchesList = (matches) => {
+    let matchesContainer = document.querySelector('.matches-container')
+    if (matchesContainer) {
+      matchesContainer.innerHTML = ''
+      const h3 = document.createElement('h3')
+      h3.innerText = 'Matches'
+      matchesContainer.appendChild(h3)
+      matches.forEach(match => {
+        const a = document.createElement('a')
+        a.href = `message.html?id=${match.id}`
+        const profileContainer = document.createElement('div')
+        profileContainer.classList.add('profile-container', 'text-dark', 'py-3')
+        const profileImage = document.createElement('div')
+        profileImage.classList.add('img-profile', 'float-left', 'rounded-circle')
+        profileImage.style.backgroundImage = `url(${match.picture})`
+        const div = document.createElement('div')
+        div.classList.add('px-2', 'float-left')
+        const h4 = document.createElement('h4')
+        h4.innerText = match.name
+        div.appendChild(h4)
+        const p = document.createElement('p')
+        p.innerText = match.description
+        div.appendChild(p)
+        profileContainer.appendChild(profileImage)
+        profileContainer.appendChild(div)
+        a.appendChild(profileContainer)
+        matchesContainer.appendChild(a)
+      });
+    }
+  }
+  let updateMyProfilePhoto = (user) => {
+    const profilePicture = document.getElementById('profile-picture')
+    profilePicture.style.backgroundImage = `url(${user.picture})`
+  }
+
+  // Get all matches
+  fetch("/api/v1/matches").then(res => {
+    console.log("Request complete! response:", res)
+    // TODO: Swap mock data with real data
+    matches = [
+      {id: 6, name: 'Terry', description: 'Loves belly rubs.', picture: '/img/dogs/dog_3.jpg'},
+      {id: 7, name: 'Florence', description: 'Loves belly rubs.', picture: '/img/dogs/dog_4.jpg'},
+      {id: 8, name: 'Roxie', description: 'Loves belly rubs.', picture: '/img/dogs/dog_5.jpg'},
+    ]
+    updateMatchesList(matches)
+  })
+  
+  // Get current profile
+  fetch("/api/v1/me").then(res => {
+    console.log("Request complete! response:", res)
+    // TODO: Swap mock data with real data
+    user = {id: 1, name: 'Cliford', description: 'Loves belly rubs.', picture: '/img/dogs/dog_1.jpg'}
+    updateMyProfilePhoto(user)
+  })
+  
+  // Get new dogs
+  fetch("/api/v1/dogs").then(res => {
+    console.log("Request complete! response:", res)
+    // TODO: Swap mock data with real data
+    dogs = [
+      {id: 2, name: 'Patches', description: 'Loves belly rubs.', picture: '/img/dogs/dog_2.jpg'},
+      {id: 3, name: 'Spot', description: 'Loves belly rubs.', picture: '/img/dogs/dog_3.jpg'},
+      {id: 4, name: 'Rover', description: 'Loves belly rubs.', picture: '/img/dogs/dog_4.jpg'},
+      {id: 5, name: 'Charles', description: 'Loves belly rubs.', picture: '/img/dogs/dog_5.jpg'},
+    ]
+    updateDogCard(dogs[0])
+  })
+  
+  // Dislike dog
+  const dislikeButton = document.getElementById('dislike')
+  if (dislikeButton) {
+    dislikeButton.addEventListener('click', (event) => {
+      if (dogs.length > 0) {
+        let dog = dogs.shift()
+        fetch(`/api/v1/dogs/${dog.id}/dislike`, {
+          method: "POST",
+        }).then(res => {
+          console.log("Request complete! response:", res)
+          updateDogCard(dogs[0])
+        })
+      }
+    })
+  }
+
+  // Like dog
+  const likeButton = document.getElementById('like')
+  if (likeButton) {
+    likeButton.addEventListener('click', (event) => {
+      if (dogs.length > 0) {
+        let dog = dogs.shift()
+        fetch(`/api/v1/dogs/${dog.id}/like`, {
+          method: "POST",
+        }).then(res => {
+          console.log("Request complete! response:", res)
+          updateDogCard(dogs[0])
+        })
+      }
+    })
+  }
+
+}
